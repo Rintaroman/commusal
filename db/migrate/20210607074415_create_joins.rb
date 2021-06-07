1699110b9f5332ca5213754076a3b0e944c8251a
Rintaroman <rintaro.584@gmail.com>
2021-06-07 16:59:46 +0900

    参加テーブル作成

--- db/migrate/20210607074415_create_joins.rb
+++ db/migrate/20210607074415_create_joins.rb
@@ -1,7 +1,8 @@
 class CreateJoins < ActiveRecord::Migration[6.0]
   def change
     create_table :joins do |t|
-
+      t.references :team        ,foreign_key: true
+      t.references :calendar    ,foreign_key: true
       t.timestamps
     end
   end
