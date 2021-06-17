3bb8ece93d726009a4dc8615a2e117fc3a11724f
Rintaroman <rintaro.584@gmail.com>
2021-06-18 02:05:45 +0900

    詳細ページはログインのみ遷移

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,5 @@
 class CalendarsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :show]
   
   def index
     @calendars = Calendar.all
