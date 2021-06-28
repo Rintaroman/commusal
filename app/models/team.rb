77cbc39401ed85a608b5d13ae2ab3887e8d9a5ec
Rintaroman <rintaro.584@gmail.com>
2021-06-28 23:35:12 +0900

    新規対戦募集レイアウト変更

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,7 @@ class Team < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user, optional: true
   belongs_to_active_hash :level
-  has_many :calendar
+  has_many :calendar, dependent: :destroy
   has_one :join
   has_many :chats
   
